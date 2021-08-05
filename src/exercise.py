97261e573a60adef8b54a837095d652d4456c8be
Justin Turner <jmturner638@gmail.com>
2021-08-05 14:36:36 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,23 @@
+import csv
+
 def main():
     #write your code below this line
+    file_name = input("Name of the file:")
+
+    with open(file_name) as f:
+        data = f.read().splitlines()
+
+    for line in data:
+        stuff = line.split(',')
+        name = stuff[0]
+        age = stuff[1]
+
+        if age == '1':
+            age = age + ' year'
+        else:
+            age = age + ' years'
+        
+        print("{}, age: {}".format(name, age))
 
 if __name__ == '__main__':
     main()
